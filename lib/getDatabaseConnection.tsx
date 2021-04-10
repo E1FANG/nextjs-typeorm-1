ff1b860cb7a2f068824db8052ecd0c7458052c19
E1FANG <438283787@qq.com>
2021-04-10 11:27:17 +0800

    成功展示posts

--- lib/getDatabaseConnection.tsx
+++ lib/getDatabaseConnection.tsx
@@ -1,36 +1,25 @@
 import {createConnection, getConnectionManager} from 'typeorm';
-import 'reflect-metadata'
-import {Post} from 'src/entity/Post'
-import {User} from 'src/entity/User'
-import {Comment} from 'src/entity/Comment'
-import config from 'ormconfig.json'
+import 'reflect-metadata';
+import {Post} from 'src/entity/Post';
+import {User} from 'src/entity/User';
+import {Comment} from 'src/entity/Comment';
+import config from 'ormconfig.json';
 
-const create = async ()=>{
+const create = async () => {
   // @ts-ignore
-  return  createConnection({
+  return createConnection({
     ...config,
-    entities:[Post,User,Comment]
-  })
-}
+    entities: [Post, User, Comment]
+  });
+};
 
-const promise = (async function (){
-  const manager = getConnectionManager()
-  const hasDefaultConnection = manager.has('default')
-  if(!hasDefaultConnection){
-    console.log('创建connection');
-    return create()
-  }else{
-    console.log('复用connection');
-    const current = manager.get('default')
-    if(current.isConnected){
-      return current
-    }else{
-      return create()
-    }
-  }
+const promise = (async function () {
+  const manager = getConnectionManager();
+  const current = manager.has('default') && manager.get('default');
+  if (current) {await current.close();}
+  return create();
+})();
 
-})()
-
-export const getDatabaseConnection = async ()=>{
-  return  promise
-}
+export const getDatabaseConnection = async () => {
+  return promise;
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,31 +6,33 @@ import {Post} from '../src/entity/Post';
 // import {User} from '../src/entity/User';
 
 type Props = {
-  posts:Post[]
+  posts: Post[]
 }
 // 前端
 const index: NextPage<Props> = (props) => {
   const {posts} = props;
   return (
     <div>
+      c12345
       {posts.map(post =>
-        <div>{post.title}</div>
+        <div key={post.id}>{post.title}</div>
       )}
     </div>
   );
 };
 export default index;
 
+
 // 后端
 export const getServerSideProps: GetServerSideProps = async (context) => {
-  const connection = await  getDatabaseConnection()
-  const posts = await connection.manager.find(Post)
+  const connection = await getDatabaseConnection();
+  const posts = await connection.manager.find(Post);
   const ua = context.req.headers['user-agent'];
   const result = new UAParser(ua).getResult();
   return {
     props: {
       browser: result.browser,
-      posts:JSON.parse(JSON.stringify(posts))
+      posts: JSON.parse(JSON.stringify(posts))
     }
   };
 };
