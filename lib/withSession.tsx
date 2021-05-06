af42b0f7331458ad67e8df51282c740505d620eb
E1FANG <438283787@qq.com>
2021-05-06 12:05:13 +0800

    消除一处ts-ignore

--- lib/withSession.tsx
+++ lib/withSession.tsx
@@ -1,12 +1,12 @@
 // this file is a wrapper with defaults to be used in both API routes and `getServerSideProps` functions
 import { withIronSession } from 'next-iron-session'
-import {NextApiHandler} from 'next';
+import {GetServerSideProps, NextApiHandler} from 'next';
 
-export  function withSession(handler :NextApiHandler) {
+export  function withSession(handler :NextApiHandler | GetServerSideProps) {
   return withIronSession(handler, {
     //password：cookies加密的密钥（防止其他人篡改）
-    // password: process.env.SECRET_COOKIE_PASSWORD,
-    password:'a011aaf5-bf82-47a1-b78f-775c9f05df75',
+    password: process.env.SECRET,
+    // password:'a011aaf5-bf82-47a1-b78f-775c9f05df75',
     cookieName: 'blog',
     cookieOptions:{secure:false}
   })
