56facb1e08d3cfa811157ba5ee6a14ea53764c3f
E1FANG <438283787@qq.com>
2021-05-06 23:50:43 +0800

    封装useForm代替Form

---
+++ hooks/useForm.tsx
@@ -0,0 +1,70 @@
+import React, {ChangeEventHandler, ReactChild, useCallback, useState} from 'react';
+
+type Field<T> = {
+  label: string,
+  type: 'text' | 'password' | 'textarea',//字符串类型？
+  // value: string | number,
+  key: keyof T
+}
+
+type useFormOptions<T> = {
+  initFormData: T;
+  fields: Field<T>[];
+  buttons: ReactChild;
+  onSubmit: (fd: T) => void
+}
+
+//泛型：T 它是一个占位符，占的是initFormData的类型的位 （所以initFormData的类型是根据外面传入的T来改变的）
+export function useForm<T>(options:useFormOptions<T>) {
+  const {initFormData,fields,buttons,onSubmit} = options
+  // 非受控组件
+  const [formData, setFormData] = useState(initFormData);
+  const [errors, setErrors] = useState(() => {
+    const e: { [k in keyof T]?: string[] } = {};
+    //e是一个对象，k肯定是T的所有值
+    // ?: 是因为 k一开始是没有的， 当遍历之后 执行e[key]=[] 才会有
+    for (let key in initFormData) {
+      if (initFormData.hasOwnProperty(key)) {
+        e[key] = [];
+      }
+    }
+    //e作为errors的初始值
+    return e;
+  });
+  const onChange = useCallback((key: keyof T, value: any) => {
+    setFormData({...formData, [key]: value});
+  }, [formData]);
+  const _onSubmit = useCallback((e) => {
+    e.preventDefault();
+    onSubmit(formData);
+  }, [onSubmit, formData]);
+  const form = (
+    <form onSubmit={_onSubmit}>
+      {fields.map(field =>
+        <div key={field.label}>
+          <label>
+            {field.label}
+            {
+              field.type === 'textarea' ?
+                <textarea onChange={e => onChange(field.key, e.target.value)}>
+                  {formData[field.key]}
+                </textarea>
+                : <input type={field.type} value={formData[field.key].toString()}
+                         onChange={e => onChange(field.key, e.target.value)}/>
+            }
+
+            {errors[field.key]?.length > 0 && <div>
+              {errors[field.key].join(',')}
+            </div>}
+          </label>
+        </div>
+      )}
+      <div>
+        {buttons}
+      </div>
+    </form>
+  );
+  return {
+    form,setErrors
+  };
+}
--- pages/posts/new.tsx
+++ pages/posts/new.tsx
@@ -1,39 +1,41 @@
 import {NextPage} from 'next';
 import React, {useCallback, useState} from 'react';
 import {Form} from '../../components/Form';
+import {useForm} from '../../hooks/useForm';
+import axios, {AxiosResponse} from 'axios';
 
-const PostsNew : NextPage = ()=>{
-  const [formData,setFormData] = useState({
-    title:'',
-    content:''
-  })
-  const [errors, setErrors] = useState({
-    title: [], content: []
-  });
-  const onChange = useCallback((key,value)=>{
-    setFormData({...formData,[key]:value})
-  },[formData])
-  const  onSubmit = useCallback((e)=>{
-    e.preventDefault();
-  },[formData])
-  return(
-    <div>
-      <Form fields={[
-        {
-          label: '标题', type: 'text', value: formData.title,
-          onChange: e=>onChange('title',e.target.value),
-          errors: errors.title
-        },
-        {
-          label: '内容', type: 'textarea', value: formData.title,
-          onChange: e=>onChange('title',e.target.value),
-          errors: errors.title
+const PostsNew: NextPage = () => {
+  //类型是静态分析，不受运行顺序影响
+  const onSubmit = (formData: typeof initFormData) => {
+    console.log('submit');
+    console.log(formData);
+    axios.post('/api/v1/posts', formData)
+      .then(() => {
+        window.alert('提交成功');
+      }, (error) => {
+        if (error.response) {
+          const response: AxiosResponse = error.response;
+          if (response.status === 422) {
+            setErrors(response.data);
+          }
         }
-      ]} onSubmit={onSubmit} buttons={<>
-        <button type='submit'>提交</button>
-      </>}/>
-    </div>
-  )
-}
+      });
+  };
+  const initFormData = {title: '', content: ''};
+  const {form, setErrors} = useForm(
+    {
+      initFormData,
+      fields: [
+        {label: '标题', type: 'text', key: 'title',},
+        {label: '内容', type: 'textarea', key: 'content'}
+      ],
+      buttons: <button>提交</button>,
+      onSubmit
+    }
+  );
+  return (
+    <div>{form}</div>
+  );
+};
 
 export default PostsNew;
