ce0d54b5c7bbe81709275dc8b271135680ca512e
E1FANG <438283787@qq.com>
2021-05-06 22:10:55 +0800

    创建posts new页面

--- pages/posts/new.tsx
+++ pages/posts/new.tsx
@@ -1,10 +1,37 @@
 import {NextPage} from 'next';
-import React from 'react';
+import React, {useCallback, useState} from 'react';
+import {Form} from '../../components/Form';
 
 const PostsNew : NextPage = ()=>{
+  const [formData,setFormData] = useState({
+    title:'',
+    content:''
+  })
+  const [errors, setErrors] = useState({
+    title: [], content: []
+  });
+  const onChange = useCallback((key,value)=>{
+    setFormData({...formData,[key]:value})
+  },[formData])
+  const  onSubmit = useCallback((e)=>{
+    e.preventDefault();
+  },[formData])
   return(
     <div>
-      <form action=""></form>
+      <Form fields={[
+        {
+          label: '标题', type: 'text', value: formData.title,
+          onChange: e=>onChange('title',e.target.value),
+          errors: errors.title
+        },
+        {
+          label: '内容', type: 'textarea', value: formData.title,
+          onChange: e=>onChange('title',e.target.value),
+          errors: errors.title
+        }
+      ]} onSubmit={onSubmit} buttons={<>
+        <button type='submit'>提交</button>
+      </>}/>
     </div>
   )
 }
