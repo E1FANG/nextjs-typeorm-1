28d9e76a374e2d5f187c737c5f05519d3350e578
E1FANG <438283787@qq.com>
2021-05-09 17:20:03 +0800

    优化首页带代码æ复用

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,41 +1,5 @@
-import {GetServerSideProps, NextPage} from 'next';
-import {UAParser} from 'ua-parser-js';
-import React, {useEffect, useState} from 'react';
-import {getDatabaseConnection} from '../lib/getDatabaseConnection';
-import {Post} from '../src/entity/Post';
-import Link from 'next/link';
-// import {User} from '../src/entity/User';
+import PostsIndex,{getServerSideProps} from './posts';
 
-type Props = {
-  posts: Post[]
-}
-// 前端
-const index: NextPage<Props> = (props) => {
-  const {posts} = props;
-  return (
-    <div>
-      <h1>文章列表</h1>
-      {posts.map(post =>
-        <Link key={post.id} href={`/posts/${post.id}`}>
-          <a>{post.title}</a>
-        </Link>
-      )}
-    </div>
-  );
-};
-export default index;
+export default PostsIndex
 
-
-// 后端
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  const connection = await getDatabaseConnection();
-  const posts = await connection.manager.find(Post);
-  const ua = context.req.headers['user-agent'];
-  const result = new UAParser(ua).getResult();
-  return {
-    props: {
-      browser: result.browser,
-      posts: JSON.parse(JSON.stringify(posts))
-    }
-  };
-};
+export {getServerSideProps}
--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,34 +1,42 @@
-import {GetStaticProps, NextPage} from 'next';
-import {usePosts} from '../../hooks/usePosts';
-import {useCallback} from 'react';
-import {getPosts} from '../../lib/posts';
+import {GetServerSideProps, NextPage} from 'next';
+import {UAParser} from 'ua-parser-js';
+import React, {useEffect, useState} from 'react';
+import {getDatabaseConnection} from 'lib/getDatabaseConnection';
+import {Post} from 'src/entity/Post';
 import Link from 'next/link';
+// import {User} from '../src/entity/User';
 
 type Props = {
-  posts: Post[];
+  posts: Post[]
 }
+// 前端
 const PostsIndex: NextPage<Props> = (props) => {
   const {posts} = props;
   return (
     <div>
       <h1>文章列表</h1>
-      {posts.map(p => <div key={p.id}>
-        <Link href={`/posts/${p.id}`}>
-          <a>
-            {p.id}
-          </a>
-        </Link>
-      </div>)}
+      {posts.map(post =>
+        <div>
+          <Link key={post.id} href={`/posts/${post.id}`}>
+            <a>{post.title}</a>
+          </Link>
+        </div>
+      )}
     </div>
   );
 };
-
 export default PostsIndex;
 
-export const getStaticProps = async () => {
-  const posts = await getPosts();
+
+// 后端
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const connection = await getDatabaseConnection();
+  const posts = await connection.manager.find(Post);
+  const ua = context.req.headers['user-agent'];
+  const result = new UAParser(ua).getResult();
   return {
     props: {
+      browser: result.browser,
       posts: JSON.parse(JSON.stringify(posts))
     }
   };
