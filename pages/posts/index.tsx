10bc742f3e7136743aee8ec64c99f55ef2ab2310
E1FANG <438283787@qq.com>
2021-05-09 17:56:49 +0800

    分页解基本完成

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -4,17 +4,20 @@ import React, {useEffect, useState} from 'react';
 import {getDatabaseConnection} from 'lib/getDatabaseConnection';
 import {Post} from 'src/entity/Post';
 import Link from 'next/link';
-// import {User} from '../src/entity/User';
+import qs from 'querystring'
 
 type Props = {
-  posts: Post[]
+  posts: Post[];
+  count:number;
+  perPage:number;
+  page:number;
 }
 // 前端
 const PostsIndex: NextPage<Props> = (props) => {
   const {posts} = props;
   return (
     <div>
-      <h1>文章列表</h1>
+      <h1>文章列表{props.count}</h1>
       {posts.map(post =>
         <div>
           <Link key={post.id} href={`/posts/${post.id}`}>
@@ -22,6 +25,12 @@ const PostsIndex: NextPage<Props> = (props) => {
           </Link>
         </div>
       )}
+      <footer>
+        共{props.count}篇文章，当前是第{props.page}页
+        <Link href={`?page=${props.page-1}`}><a>上一页</a></Link>
+        |
+        <Link href={`?page=${props.page+1}`}><a>下一页</a></Link>
+      </footer>
     </div>
   );
 };
@@ -30,14 +39,19 @@ export default PostsIndex;
 
 // 后端
 export const getServerSideProps: GetServerSideProps = async (context) => {
+  const index = context.req.url.indexOf('?')
+  const search = context.req.url.substr(index+1)
+  const query = qs.parse(search)
+  const page = parseInt(query.page.toString()) || 1
+  const perPage = 3
   const connection = await getDatabaseConnection();
-  const posts = await connection.manager.find(Post);
-  const ua = context.req.headers['user-agent'];
-  const result = new UAParser(ua).getResult();
+  const [posts,count] = await connection.manager.findAndCount(Post,
+    {skip:(page-1)*perPage,take:perPage});
   return {
     props: {
-      browser: result.browser,
-      posts: JSON.parse(JSON.stringify(posts))
+      posts: JSON.parse(JSON.stringify(posts)),
+      count,
+      perPage,page
     }
   };
 };
