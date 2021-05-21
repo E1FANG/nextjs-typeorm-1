8b8696992598d3f78c916c6f88cc3f3cb8e07223
E1FANG <438283787@qq.com>
2021-05-21 13:00:54 +0800

    完善博客编辑删除等功能,增加页面å¤样式;

--- hooks/useForm.tsx
+++ hooks/useForm.tsx
@@ -1,37 +1,36 @@
-import React, { ReactChild, useCallback, useState} from 'react';
+import {ChangeEventHandler, ReactChild, useCallback, useState} from 'react';
 import {AxiosResponse} from 'axios';
+import cs from 'classnames';
 
 type Field<T> = {
-  label: string,
-  type: 'text' | 'password' | 'textarea',//字符串类型？
-  key: keyof T
+  label: string;
+  type: 'text' | 'password' | 'textarea';
+  key: keyof T;
+  className?: string;
 }
-
 type useFormOptions<T> = {
   initFormData: T;
   fields: Field<T>[];
   buttons: ReactChild;
   submit: {
-    request:(formData:T) => Promise<AxiosResponse<T>>,
-    success:()=>void
+    request: (formData: T) => Promise<AxiosResponse<T>>;
+    success: () => void;
   }
 }
 
-//泛型：T 它是一个占位符，占的是initFormData的类型的位 （所以initFormData的类型是根据外面传入的T来改变的）
-export function useForm<T>(options:useFormOptions<T>) {
-  const {initFormData,fields,buttons,submit} = options
-  // 非受控组件
+export function useForm<T>(options: useFormOptions<T>) {
+  const {initFormData, fields, buttons, submit} = options;
+  // 非受控
   const [formData, setFormData] = useState(initFormData);
+  // initFormData = {username:'', password:''}
+  // initErrors = {username: [], password: []}
   const [errors, setErrors] = useState(() => {
     const e: { [k in keyof T]?: string[] } = {};
-    //e是一个对象，k肯定是T的所有值
-    // ?: 是因为 k一开始是没有的， 当遍历之后 执行e[key]=[] 才会有
     for (let key in initFormData) {
-      if (initFormData.hasOwnProperty(key)) {
+      if (initFormData.hasOwnProperty(key)) { // 为了严谨
         e[key] = [];
       }
     }
-    //e作为errors的初始值
     return e;
   });
   const onChange = useCallback((key: keyof T, value: any) => {
@@ -39,17 +38,16 @@ export function useForm<T>(options:useFormOptions<T>) {
   }, [formData]);
   const _onSubmit = useCallback((e) => {
     e.preventDefault();
-    submit.request(formData).then(
-      submit.success,
+    submit.request(formData).then(submit.success,
       (error) => {
         if (error.response) {
           const response: AxiosResponse = error.response;
           if (response.status === 422) {
             setErrors(response.data);
-          }else if (response.status === 401){
-            window.alert('没有登录')
+          } else if (response.status === 401) {
+            window.alert('请先登录');
             window.location.href =
-              `/sign_in?returnTo=${encodeURI(window.location.pathname)}`
+              `/sign_in?returnTo=${encodeURIComponent(window.location.pathname)}`;
           }
         }
       }
@@ -59,30 +57,52 @@ export function useForm<T>(options:useFormOptions<T>) {
   const form = (
     <form onSubmit={_onSubmit}>
       {fields.map(field =>
-        <div key={field.label}>
-          <label>
-            {field.label}
-            {
-              field.type === 'textarea' ?
-                <textarea onChange={e => onChange(field.key, e.target.value)}
-                          value={formData[field.key].toString()} />
-                :
-                <input type={field.type} value={formData[field.key].toString()}
-                         onChange={e => onChange(field.key, e.target.value)}/>
+        <div key={field.key.toString()}
+             className={cs('field', `field-${field.key}`, field.className)}>
+          <label className="label">
+            <span className="label-text">
+              {field.label}
+            </span>
+            {field.type === 'textarea' ?
+              <textarea className="control"
+                        onChange={(e) => onChange(field.key, e.target.value)}
+                        value={formData[field.key].toString()}/>
+              :
+              <input className="control"
+                     type={field.type} value={formData[field.key].toString()}
+                     onChange={(e) => onChange(field.key, e.target.value)}/>
             }
-
-            {errors[field.key]?.length > 0 && <div>
-              {errors[field.key].join(',')}
-            </div>}
           </label>
+          {errors[field.key]?.length > 0 && <div>
+            {errors[field.key].join(',')}
+          </div>}
         </div>
       )}
       <div>
         {buttons}
       </div>
+      <style jsx>{`
+      .field{
+        margin: 8px 0;
+      }
+      .label{
+        display:flex;
+        line-height: 32px;
+      } 
+      .label input {
+        height: 32px;
+      }
+      .label > .label-text{
+        white-space: nowrap;
+        margin-right: 1em;
+      }
+      .label > .control{
+        width: 100%;
+      }
+      `}</style>
     </form>
   );
   return {
-    form,setErrors
+    form: form, setErrors: setErrors
   };
 }
--- pages/posts/new.tsx
+++ pages/posts/new.tsx
@@ -1,29 +1,49 @@
 import {NextPage} from 'next';
-import React from 'react';
-import {useForm} from '../../hooks/useForm';
 import axios from 'axios';
+import {useForm} from '../../hooks/useForm';
 
 const PostsNew: NextPage = () => {
-  //类型是静态分析，不受运行顺序影响
-  const {form} = useForm(
-    {
-      initFormData:{title: '', content: ''},
-      fields: [
-        {label: '标题', type: 'text', key: 'title',},
-        {label: '内容', type: 'textarea', key: 'content'}
-      ],
-      buttons: <button>提交</button>,
-      submit:{
-        request:formData => axios.post(`/api/v1/posts`,formData),
-        success:()=> {
-          window.alert('提交成功')
-          window.location.href = '/posts'
-        }
+  const {form} = useForm({
+    initFormData: {title: '', content: ''},
+    fields: [
+      {label: '大标题', type: 'text', key: 'title'},
+      {label: '内容', type: 'textarea', key: 'content'},
+    ],
+    buttons: <div className="actions">
+      <button type="submit">提交</button>
+    </div>,
+    submit: {
+      request: formData => axios.post(`/api/v1/posts`, formData),
+      success: () => {
+        window.alert('提交成功');
+        window.location.href = '/posts';
       }
     }
-  );
+  });
   return (
-    <div>{form}</div>
+    <div className="postsNew">
+      <div className="form-wrapper">
+        {form}
+      </div>
+      <style jsx global>{`
+      .form-wrapper{
+        padding: 16px;
+      }
+      .postsNew .field-content textarea{
+        min-height: 20em; 
+        resize: none;
+      }
+      .postsNew .label-text{
+        width: 4em;
+        text-align:right;
+      }
+      .postsNew .actions{
+        text-align:center;
+        background: #ddd;
+        padding: 4px 0;
+      }
+      `}</style>
+    </div>
   );
 };
 
