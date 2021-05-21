3ee378e2f6014c62841d052251a3bf4d5bd70b62
E1FANG <438283787@qq.com>
2021-05-21 18:48:03 +0800

    修改deploy.sh

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,8 +1,13 @@
+echo 'start';
 docker start 923 &&
 cd /home/blog/app/ &&
 git pull &&
 yarn install --production=false &&
 yarn build &&
+git apply migrate.patch;
+yarn compile &&
+yarn m:run &&
+git reset --hard HEAD &&
 docker build  -t hasson/node-web-app . &&
 docker kill app &&
 docker rm app &&
