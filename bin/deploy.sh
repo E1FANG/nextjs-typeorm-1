ef7ee39fe8feff3aeba1c7b948ad34238db22e55
E1FANG <438283787@qq.com>
2021-05-21 17:36:18 +0800

    修改主页

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,1 +1,10 @@
-echo Hello > /tmp/hasson
+docker start 923 &&
+cd /home/blog/app/ &&
+git pull &&
+yarn install --production=false &&
+yarn build &&
+docker build -t hasson/node-web-app &&
+docker kill app &&
+docker rm app &&
+docker run --name app --network=host  -p 3000:30000 -d hasson/node-web-app &&
+echo 'OK!'
