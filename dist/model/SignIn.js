446f8833696aa6ab724d243ff85eab3a03b0dcd0
e1fang <438283787@qq.com>
2024-04-10 05:44:45 -0400

    fix: login user validate

--- dist/model/SignIn.js
+++ dist/model/SignIn.js
@@ -17,7 +17,12 @@ var _createClass2 = _interopRequireDefault(require("@babel/runtime/helpers/creat
 
 var _defineProperty2 = _interopRequireDefault(require("@babel/runtime/helpers/defineProperty"));
 
-// import {getDatabaseConnection} from '../../lib/getDatabaseConnection';
+var _getDatabaseConnection = require("../../lib/getDatabaseConnection");
+
+var _User = require("../entity/User");
+
+var _md = _interopRequireDefault(require("md5"));
+
 var SignIn = /*#__PURE__*/function () {
   function SignIn() {
     (0, _classCallCheck2["default"])(this, SignIn);
@@ -34,25 +39,40 @@ var SignIn = /*#__PURE__*/function () {
     key: "validate",
     value: function () {
       var _validate = (0, _asyncToGenerator2["default"])( /*#__PURE__*/_regenerator["default"].mark(function _callee() {
+        var connection, user;
         return _regenerator["default"].wrap(function _callee$(_context) {
           while (1) {
             switch (_context.prev = _context.next) {
               case 0:
                 if (this.username.trim() === '') {
                   this.errors.username.push('请填写用户名');
-                } // const connection = await getDatabaseConnection();
-                // const user = await connection.manager.findOne(User, {where: {username: this.username}});
-                // this.user = user;
-                // if (user) {
-                //   if (user.passwordDigest !== md5(this.password)) {
-                //     this.errors.password.push('密码与用户名不匹配');
-                //   }
-                // } else {
-                //   this.errors.username.push('用户名不存在');
-                // }
+                }
 
+                _context.next = 3;
+                return (0, _getDatabaseConnection.getDatabaseConnection)();
 
-              case 1:
+              case 3:
+                connection = _context.sent;
+                _context.next = 6;
+                return connection.manager.findOne(_User.User, {
+                  where: {
+                    username: this.username
+                  }
+                });
+
+              case 6:
+                user = _context.sent;
+                this.user = user;
+
+                if (user) {
+                  if (user.passwordDigest !== (0, _md["default"])(this.password)) {
+                    this.errors.password.push('密码与用户名不匹配');
+                  }
+                } else {
+                  this.errors.username.push('用户名不存在');
+                }
+
+              case 9:
               case "end":
                 return _context.stop();
             }
